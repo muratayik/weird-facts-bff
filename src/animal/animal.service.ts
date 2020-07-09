9dd09880094aefd60b5fe87a923d697a89179203
Murat AYIK <muratayik@gmail.com>
2020-07-09 21:05:17 +0200

    Move faker to bff

--- src/animal/animal.service.ts
+++ src/animal/animal.service.ts
@@ -1,5 +1,6 @@
 import { Injectable, BadRequestException } from '@nestjs/common';
 import Axios from 'axios';
+import faker from 'faker';
 import { AnimalTypes } from './animal.types.enum';
 
 @Injectable()
@@ -15,6 +16,16 @@ export class AnimalService {
       `https://cat-fact.herokuapp.com/facts?animal_type=${animalType}`,
     );
 
-    return response.data;
+    return response.data.all.map(animal => {
+      const { _id, text, type } = animal;
+      return {
+        _id,
+        text,
+        type,
+        userName: faker.name.findName(),
+        avatar: faker.image.avatar(),
+        postingDate: faker.date.past().toDateString(),
+      };
+    });
   }
 }
